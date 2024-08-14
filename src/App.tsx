51f4d06edbd542ed09cfd4fee09c7b0e10717af6
noneuser04 <y274835@gmail.com>
2024-08-14 09:34:06 +0900

    first

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+           <code>Result</code> 
+        </p>
+        <img src={process.env.PUBLIC_URL+"/img/dollor.jpg"}/>
+        <p>
+           <code>You get 10000 bucks</code> 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
